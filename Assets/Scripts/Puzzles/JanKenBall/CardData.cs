3841d689450d07b40d84a2b00f043c478714d94e
ElviejoRhae <65338439+ElviejoRhae@users.noreply.github.com>
2022-04-19 20:10:23 +0200

    card selector

--- Assets/Scripts/Puzzles/JanKenBall/CardData.cs
+++ Assets/Scripts/Puzzles/JanKenBall/CardData.cs
@@ -10,8 +10,14 @@ public class CardData : MonoBehaviour, IPointerClickHandler
     [SerializeField] private bool isSelected;
     [SerializeField] private JanKenController janKenController;
 
+    private void Awake()
+    {
+        janKenController = GameObject.FindGameObjectWithTag("JanKenBallController").GetComponent<JanKenController>();
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        
+        janKenController.PickaCard(cardID);
     }
+
 }
--- Assets/Scripts/Puzzles/JanKenBall/JanKenController.cs
+++ Assets/Scripts/Puzzles/JanKenBall/JanKenController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class JanKenController : MonoBehaviour
 {
+
+    [SerializeField] private CardType selectedCard;
+    [SerializeField] private CardType chosenCard;
+
     void Start()
     {
         
@@ -13,4 +17,21 @@ public class JanKenController : MonoBehaviour
     {
         
     }
+
+    public void PickaCard(CardType card)
+    {
+        if (selectedCard != card)
+        {
+            Debug.Log("He pre-seleccionado la carta " + card);
+            selectedCard = card;
+        }
+
+        else
+        {
+            Debug.Log("He elegido la carta " + card);
+            chosenCard = card;
+        }
+
+    }
+
 }
