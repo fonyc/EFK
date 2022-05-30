a0255caa4fc5d46db8834ff8cd339d1847610f96
fonyc <fonycas@hotmail.com>
2022-05-30 04:37:22 +0200

    better probes in lights in library and HUD changes. added UI packages

---
+++ Assets/Art/UI_Packs/CryptUI/Scripts/Demo/TickTimer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CryptUI.Scripts.Demo
+{
+    public class TickTimer : MonoBehaviour
+    {
+        public Text text;
+
+        public void SetText(string value)
+        {
+            text.text = value;
+        }
+    }
+}
